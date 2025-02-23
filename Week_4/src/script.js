006707050dcb0e04465d3859dde7804a205845dd
Ramchandru-45 <ramchan452004@gmail.com>
2025-02-24 02:07:04 +0530

    webPage First Comit

--- Week_4/src/script.js
+++ Week_4/src/script.js
@@ -8,19 +8,32 @@ function addRoutine(){
     document.getElementById('table').appendChild(tr);
 }
 
+function fileReader(){
+    const fs = require('fs');
+    fs.readFile('Input.txt', (err, data) => {
+    if (err) throw err;
+
+    console.log(data.toString());
+    });
+}
 
 function homePage(){
     const home=document.querySelector('.home');
     const routine=document.querySelector('.routine');
+    const create=document.querySelector('.create');
     home.classList.add('active');
     routine.classList.remove('active');
+    create.classList.remove('active');
 }
 
 function routinePage(){
+    //fileReader();
     const home=document.querySelector('.home');
     const routine=document.querySelector('.routine');
+    const create=document.querySelector('.create');
     routine.classList.add('active');
     home.classList.remove('active');
+    create.classList.remove('active');
 }
 
 function trackPage(){
@@ -28,5 +41,10 @@ function trackPage(){
 }
 
 function createPage(){
-
+    const home=document.querySelector('.home');
+    const routine=document.querySelector('.routine');
+    const create=document.querySelector('.create');
+    routine.classList.remove('active');
+    home.classList.remove('active');
+    create.classList.add('active');
 }
